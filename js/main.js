500130404f6c9cbda5c8a07007d647a3f6303f5c
Munmud <moontasir042@gmail.com>
2020-08-25 13:31:28 +0600

    webfont deleted

--- js/main.js
+++ js/main.js
@@ -9,15 +9,19 @@ let ticketPrice = +movieSelect.val() ;
 //Update total and count
 function updatedSelectedCount(){
     const selectedSeats = $(".row .seat.selected") ;
+
+    const seatsIndex = [...selectedSeats] ;
+
     const selectedSeatsCount = selectedSeats.length ;
     count.text(selectedSeatsCount)
     total.text(selectedSeatsCount * ticketPrice) ;
 }
-// Seat click event
+// Select change
 movieSelect.on("change" , e =>{
     ticketPrice = +e.target.value ;
     updatedSelectedCount() ;
 }) ;
+
 container.on("click" , (e) => {
     if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected') ;
